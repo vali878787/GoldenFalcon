6ce6f984a2ab4357a0a78249a94ea93e2409bdf8
vali <valinorozib1@gmail.com>
2026-07-02 12:52:49 +0330

    fix whatsapp link issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -41,13 +41,15 @@ export default function Footer() {
 
           <p className="mt-2 text-sm text-gray-300">
   WhatsApp:{" "}
-  <span 
-    onClick={() => window.open("https://wa.me/971502845790", "_blank")}
-    className="text-[#D4AF37] hover:underline cursor-pointer"
+  <a 
+    href="https://wa.me/971502845790" 
+    target="_blank" 
+    rel="noopener noreferrer"
   >
     +971 50 284 5790
-  </span>
+  </a >
 </p >
+
 
 
 
