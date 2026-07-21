42ba8eed1b5f238b31225c7fed90f129230a852e
vali <valinorozib1@gmail.com>
2026-07-21 20:42:27 +0330

    Fix locale navigation and exclude admin from i18n middleware

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,10 @@
-import createMiddleware from 'next-intl/middleware';
-import { routing } from './i18n/routing';
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
 
 export default createMiddleware(routing);
 
 export const config = {
   matcher: [
-    '/((?!api|_next|.*\\..*).*)',
+    "/((?!api|admin|_next|.*\\..*).*)",
   ],
 };
